7bf31504809ec420c3664270b7568169cc8cdd85
Juanisa McCoy <juanisa.j.mccoy@nasa.gov>
2026-08-19 20:12:08 -0400

    update os theme for visibility

--- src/views/components/TerraProvider.tsx
+++ src/views/components/TerraProvider.tsx
@@ -1,5 +1,6 @@
 import { setBasePath } from "@nasa-terra/components/dist/utilities/base-path.js";
 import type { ReactNode } from "react";
+import { useEffect } from "react";
 
 setBasePath("https://cdn.jsdelivr.net/npm/@nasa-terra/components@latest/cdn/");
 
@@ -7,5 +8,9 @@ setBasePath("https://cdn.jsdelivr.net/npm/@nasa-terra/components@latest/cdn/");
  * A wrapper over Terra UI components to ease the usage MCP UI
  */
 export default function TerraProvider(props: { children: ReactNode }) {
+  useEffect(() => {
+    document.documentElement.classList.add("terra-prefers-color-scheme");
+  }, []);
+  
   return <>{props.children}</>;
 }
